b02de1b79462e4dac3c0f5f5aa78dac04cb509e6
Paulo Henrique COngentino Ten <phcten@gmail.com>
2021-03-07 19:04:24 -0300

    Added: FindById method and exception handling

--- src/main/java/com/pauloten/workshopmongodbunity/Services/UserService.java
+++ src/main/java/com/pauloten/workshopmongodbunity/Services/UserService.java
@@ -2,10 +2,12 @@ package com.pauloten.workshopmongodbunity.Services;
 
 import com.pauloten.workshopmongodbunity.Domain.User;
 import com.pauloten.workshopmongodbunity.Repository.UserRepository;
+import com.pauloten.workshopmongodbunity.Services.Exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repos.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repos.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
