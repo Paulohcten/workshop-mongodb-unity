1c45f93412a0585eb8799f4804d4af7b214e08c2
Paulo Henrique COngentino Ten <phcten@gmail.com>
2021-03-07 15:32:43 -0300

    Project created

---
+++ src/main/java/com/pauloten/workshopmongodbunity/WorkshopMongodbUnityApplication.java
@@ -0,0 +1,13 @@
+package com.pauloten.workshopmongodbunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongodbUnityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopMongodbUnityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pauloten/workshopmongodbunity/WorkshopMongodbUnityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloten.workshopmongodbunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbUnityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
