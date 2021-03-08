e8854b0ff7e3498a919fcec3b5bef4ad2a24c0bf
Paulo Henrique COngentino Ten <phcten@gmail.com>
2021-03-07 23:46:48 -0300

    Simple query using Query method

--- src/main/java/com/pauloten/workshopmongodbunity/Resources/PostResource.java
+++ src/main/java/com/pauloten/workshopmongodbunity/Resources/PostResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping(value = "/Posts")
@@ -15,8 +17,13 @@ public class PostResource {
     private PostService service;
 
 
+    @RequestMapping(value = "/{id}/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "") String text){
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Post> findById(@PathVariable String id){
+    public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
